4267d2a45475bdecd7523e2d73eb0700dc472413
Aiub Duduev <aub.duduev.1989@gmail.com>
2022-12-25 09:28:23 +0300

    rename ViewBuilder

--- Sources/BVVManager/BuilderProtocol.swift
+++ Sources/BVVManager/BuilderProtocol.swift
@@ -1,12 +1,12 @@
 //
-//  ViewBuilderProtocol.swift
+//  BuilderProtocol.swift
 //  
 //
 //  Created by Senior Developer on 10.09.2022.
 //
 import UIKit
 
-public protocol ViewBuilderProtocol: AnyObject {
+public protocol BuilderProtocol: AnyObject {
     
     associatedtype V : ViewProtocol
     associatedtype VS: ViewManager<V>
